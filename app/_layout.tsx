daf7af245d68bb489dad1d5cde1fc4fd36dfc918
jobayedislam <islam.mjobayed@gmail.com>
2026-04-22 22:54:45 +0600

    feat: wrap layout with PaperProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
 import { Stack } from "expo-router";
+import { PaperProvider } from "react-native-paper";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <PaperProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </PaperProvider>
   );
 }
